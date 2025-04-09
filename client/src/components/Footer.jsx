f406f9b6fea6a409da72ec5a99e412759774b696
ShrivastavaAyush1611 <ayushshrivastava16012005@gmail.com>
2025-04-09 05:44:22 +0530

    CHECKING

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='text-sm text-center bg-gray-50 py-3 '>
-      Made By Ayush Shrivastava
+      Made by Ayush Shrivastava
     </div>
   )
 }
